a9de9092dea9fae93cf81fbaee670bed1a9d2b6a
riktadhali <riktadhali031@gmail.com>
2024-11-19 14:15:04 +0600

    first commit

---
+++ todos/t.js
@@ -0,0 +1,27 @@
+function loaddata(){
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(data => displaydata(data))
+    
+  }
+    loaddata();
+  const displaydata = (data) =>{
+    const coldata = document.getElementById("todos");
+        data.forEach((post) => {
+        
+          const postDiv = document.createElement("div");
+
+            postDiv.innerHTML = `
+            
+             <p>Rikta
+             </p>
+
+             
+                `;
+             coldata.appendChild(postDiv);         
+
+
+        });
+
+
+  }
